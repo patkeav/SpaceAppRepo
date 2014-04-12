047966a426f75e0f2f6e27bf18abfdb108574e3a
Leonardo Ascione <me@leonardfactory.com>
2014-04-12 19:52:05 +0200

    Base font styling

--- storytelling/index.php
+++ storytelling/index.php
@@ -2,11 +2,14 @@
 	<head>
 		<script type='text/javascript' src='resources/js/google-ajax.min.js'></script>
 		<script type='text/javascript' src='resources/js/jquery.min.js'></script>
-		<link href="resources/fonts/bebas.css" rel="stylesheet" />
+		<link href="resources/fonts/bebas/bebas.css" rel="stylesheet" />
 		<link href="resources/css/style.css" rel="stylesheet" />
 	</head>
 	<body class="container">
-		
+		<header>
+			<h1>Asteroids</h1>
+			<h2>On steroids</h2>
+		</header>
 	</body>
 </html>
 
