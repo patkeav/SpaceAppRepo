40ae4981171ae8adb7d2415264a4ae1c435bad30
Leonardo Ascione <me@leonardfactory.com>
2014-04-12 19:39:20 +0200

    Right folder for betas

--- storytelling/index.php
+++ storytelling/index.php
@@ -2,9 +2,7 @@
 	<head>
 		<script type='text/javascript' src='resources/js/google-ajax.min.js'></script>
 		<script type='text/javascript' src='resources/js/jquery.min.js'></script>
-		<script src="resources/js/bootstrap.min.js"></script>
-		<link href="resources/css/bootstrap.css" rel="stylesheet" />
-		<link href="resources/css/bootstrap-theme.min.css" rel="stylesheet" />
+		<link href="resources/fonts/bebas.css" rel="stylesheet" />
 		<link href="resources/css/style.css" rel="stylesheet" />
 	</head>
 	<body class="container">
